3b2cb749420b68eb4557d4170ed16318e0083d1d
Amirzhan <albert.git1@gmail.com>
2022-12-18 23:10:29 +0500

    Добавить API для работы с версиями плагина

--- src/PluginStore.Api/Controllers/PluginController.cs
+++ src/PluginStore.Api/Controllers/PluginController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PluginStore.Api.Data;
@@ -35,7 +36,7 @@ public class PluginController : ControllerBase
     [HttpGet]
     public async Task<IActionResult> GetAll()
     {
-        var plugins = await _dbContext.Plugins.Include(v => v.PluginVersions).ToListAsync();
+        var plugins = await _dbContext.Plugins.ToListAsync();
 
         return Ok(plugins);
     }
@@ -51,5 +52,80 @@ public class PluginController : ControllerBase
         }
 
         return Ok(plugin);
+    }
+
+    [HttpPost("versions/upload")]
+    [Authorize]
+    public async Task<IActionResult> Upload([FromForm] UploadPluginDto uploadDto)
+    {
+        var plugin = await _dbContext.Plugins.FirstOrDefaultAsync(p => p.PluginId == uploadDto.PluginId);
+
+        if (plugin == null)
+        {
+            return BadRequest("Плагин не найден");
+        }
+        
+        var userName = User.Claims.FirstOrDefault(c => c.Type == "name")?.Value!;
+
+        var pluginVersion = new PluginVersion
+        {
+            Version = uploadDto.Version,
+            Description = uploadDto.Description,
+            Author = userName,
+            GitLink = uploadDto.GitLink,
+            Beta = uploadDto.Beta
+        };
+        
+        plugin.PluginVersions.Add(pluginVersion);
+        await _dbContext.SaveChangesAsync();
+
+        return Ok(pluginVersion);
+    }
+
+    [HttpPost, HttpDelete]
+    [Route("versions/{pluginVersionId}/beta")]
+    public async Task<IActionResult> SwitchBeta(int pluginVersionId)
+    {
+        var pluginVersion = await _dbContext.PluginVersions.FirstOrDefaultAsync(p => p.PluginVersionId == pluginVersionId);
+        
+        if (pluginVersion == null)
+        {
+            return NotFound("Плагин не найден");
+        }
+
+        pluginVersion.Beta = Request.Method == HttpMethods.Post;
+        await _dbContext.SaveChangesAsync();
+        return Ok(pluginVersion);
+    }
+    
+    [HttpPost, HttpDelete]
+    [Route("versions/{pluginVersionId}/deprecated")]
+    public async Task<IActionResult> SwitchDeprecated(int pluginVersionId)
+    {
+        var pluginVersion = await _dbContext.PluginVersions.FirstOrDefaultAsync(p => p.PluginVersionId == pluginVersionId);
+        
+        if (pluginVersion == null)
+        {
+            return NotFound("Плагин не найден");
+        }
+
+        pluginVersion.Deprecated = Request.Method == HttpMethods.Post ? DateTime.Now : null;
+        await _dbContext.SaveChangesAsync();
+        return Ok(pluginVersion);
+    }
+
+    [HttpGet("versions/{pluginId}/current")]
+    public async Task<IActionResult> CurrentVersion(int pluginId)
+    {
+        var plugin = await _dbContext.Plugins.Include(v => v.PluginVersions).FirstOrDefaultAsync(p => p.PluginId == pluginId);
+
+        if (plugin == null)
+        {
+            return NotFound("Плагин не найден");
+        }
+
+        var currentVersion = plugin.PluginVersions.LastOrDefault();
+
+        return Ok(currentVersion);
     }
 }
